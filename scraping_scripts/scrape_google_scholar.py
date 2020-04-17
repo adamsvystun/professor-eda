be8cd4f2a6056ebd75264423f19d3459d06ca516
Adam Svystun <adam.svystun@gmail.com>
2020-04-17 14:34:00 -0400

    Update googlescholar data

--- scraping_scripts/scrape_google_scholar.py
+++ scraping_scripts/scrape_google_scholar.py
@@ -23,8 +23,9 @@ location = '.cache/google_scholar'
 memory = Memory(location, verbose=1)
 
 
-def memory_cache(*args):
+def memory_cache(*args, **kwargs):
     recalculate_none = False
+    cache_only = False
     def _memory_cache(f):
         cache_file = f.__name__ + '.json'
         print(cache_file)
@@ -42,6 +43,8 @@ def memory_cache(*args):
                 if res or (not res and not recalculate_none):
                     return res
                 # print('[>] Recalclating')
+            if cache_only:
+                return None
             res = f(arg)
 
             cache[arg] = res
@@ -55,7 +58,9 @@ def memory_cache(*args):
     if len(args) == 1 and callable(args[0]):
         return _memory_cache(args[0])
     else:
-        recalculate_none = args[0]
+        if len(args) >= 1:
+            recalculate_none = args[0]
+        cache_only = kwargs['cache_only']
         return _memory_cache
 
 
@@ -109,7 +114,7 @@ class CaptchaPresent(Exception):
 
 captcha_regex = re.compile(r"www.google.com/recaptcha/api.js")
 
-@memory_cache
+@memory_cache(cache_only=True)
 def find_author_link(author):
     def check_captcha(soup):
         print(soup)
@@ -137,7 +142,7 @@ def find_author_link(author):
     return "http://scholar.google.com" + h4.a.get('href')
 
 
-@memory_cache
+@memory_cache(cache_only=True)
 def scrape_link(link):
     def check_captcha(soup):
         are_results_on_page = soup.find('div', class_='gsc_prf_il') is not None
@@ -165,22 +170,30 @@ def scrape_google_scholar(options):
 
     # Create a dataframe where to save results
     gs_data = pd.DataFrame(
-        columns=['name', 'affiliation', 'citations', 'hindex', 'i10index'])
+        columns=[
+            'name', 'affiliation', 'citations', 'hindex', 'i10index', 'quality',
+            'n_ratings', 'easiness', 'department', 'college'
+        ]
+    )
     number_of_authors_not_found = 0
 
     # Go through each professor
-    for professor_name in tqdm(professors['Name']):
+    for i, row in tqdm(professors.iterrows()):
         try:
-            link = find_author_link(str(professor_name))
+            link = find_author_link(str(row['Name']))
             if not link:
                 continue
             data = scrape_link(link)
-            data['name'] = professor_name
+            if not data:
+                continue
+            data['name'] = row['Name']
+            data['quality'] = row['Overall Quality']
+            data['n_ratings'] = row['Total Ratings'] 
+            data['easiness'] = row['Easiness']
+            data['department'] = row['Department']
+            data['college'] = row['College']            
             gs_data = gs_data.append(data, ignore_index=True)
         except KeyboardInterrupt:
-            break
-        except Exception as e:
-            print(e)
             break
         # Sleep for 1 second so google doesn't mark as a bot
         # time.sleep(1)
